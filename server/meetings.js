7d41b967b7480e90e3eccee0266a2bd6a5978bb4
JJUAREZZ <joacojuarez1@gmail.com>
2022-02-05 11:34:11 -0300

    meetings routes

--- server/meetings.js
+++ server/meetings.js
@@ -1,3 +1,29 @@
 const meetingsRouter = require("express").Router();
 
 module.exports = meetingsRouter;
+
+const {
+  addToDatabase,
+  getAllFromDatabase,
+  deleteAllFromDatabase,
+  createMeeting,
+} = require("./db");
+
+meetingsRouter.get("/", (req, res, next) => {
+  res.send(getAllFromDatabase("meetings"));
+});
+
+meetingsRouter.post("/", (req, res, next) => {
+  const newMeeting = addToDatabase("meetings", createMeeting());
+  res.status(201).send(newMeeting);
+});
+
+meetingsRouter.delete("/", (req, res, next) => {
+  const deleteMeeting = deleteAllFromDatabase("meetings", req.params.id);
+  if (deleteMeeting) {
+    res.status(204);
+  } else {
+    res.status(500);
+  }
+  res.send();
+});
